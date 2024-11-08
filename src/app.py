4e1e8271d00e7f68b84b6e9cec260a9d23f02128
vikasjangid100 <vikas.jangid.102006@gmail.com>
2024-11-08 18:54:31 +0530

    four

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("../insurence.csv")
+df = pd.read_csv("insurence.csv")
 
 print(df.head(5))
 
